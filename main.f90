5de26d65db9663b5bfe9adc5289aa6390f9564b3
morniehtar <46871606+morniehtar@users.noreply.github.com>
2022-06-21 02:40:40 +0300

    ver. 1.1

--- main.f90
+++ main.f90
@@ -3,19 +3,17 @@ module config
     public
 
     !Integration precision (also affects smoothness)
-    integer, parameter :: dots = 100000 ! 100000 is precise and fast
-    !Threads count
-    !integer, parameter :: thr = 4
+    integer, parameter :: dots = 1000 ! 100000 is precise and fast
 !---------------------------------------------------------------
     !Effective infinity
-    real(8), parameter :: xinf = 10.d0 ! Epsilon is 1.d-16, U(x)~0 for x=1.d1
-    !Crosslinking point
+    real(8), parameter :: xinf = 10.d0 ! Epsilon is 1.d-16, U(x)~0 for x=10.d0
+    !Cross-linking point
     real(8), parameter :: xcrs = -0.5d0
     !Energy seeking break limit
     real(8), parameter :: einf = 10.d0
     !Machine epsilon (bisection limiter)
     real(8), parameter :: eps = 1.d-9
-    !
+    !Energy search width
     real(8), parameter :: estep = 1.d0
 !---------------------------------------------------------------
 end module config
@@ -31,7 +29,7 @@ module potent
     end interface
 
     !Current U(x) function
-    procedure(ufct), pointer :: uptr => U0
+    procedure(U0), pointer :: uptr => U1
 
 contains
 
@@ -61,158 +59,81 @@ program main
     use potent
     implicit none
 
-    procedure(getLog), pointer :: ptr
     real(8) :: yleft, zleft, yright, zright
-    integer :: i, lim
-    real(8) :: eBt, eTp, eMid, stBot, stMid, stE
     real(8) :: erg
 
-    !erg = -8.0004364013671960d0
-    !erg = -4.5006561279297079d0
-    !erg = -2.0004974365234567d0
-    !erg = -0.50028381347658124d0
-    !erg = -0.592d0
+    erg = -0.592d0
 
-    lim = einf/estep
-    ptr => getLog
+    call integL(erg, yleft, zleft)
+    call integR(erg, yright, zright)
 
-    do i = 1, lim+1
-        eBt = -einf + (i-1)*estep
-        eTp = -einf + i*estep
-        call bisection(ptr, eBt, eTp)
-    end do
-
-    !print *, "Log derivatives:", getLog(yleft, zleft, yright, zright)
-    !print *, "Wronsky:", getWron(yleft, zleft, yright, zright)
-
+    print *, getLog(yleft, zleft, yright, zright)
 
 contains
-
-    subroutine bisection(fptr, a, b)
-        !Please, ensure that the function is monotoneous, otherwise
-        !the criteria f(a)*f(b)<0 doesn't really make that much sense
-        !The a and b aren't initial guesses, those are the borders
-        implicit none
-
-        abstract interface
-            real(8) function ftst(x)
-            real(8), intent(in) :: x
-            end function
-        end interface
-
-        procedure(ftst), pointer, intent(in) :: fptr
-        real(8), intent(in) :: a, b
-        real(8) :: bt, md, tp
-        integer :: j
-
-        if (fptr(a) * fptr(b) .gt. 0) then
-            print *, "Bisection error: root criteria isn't met"
-            return
-        end if
-
-        bt = a
-        tp = b
-        md = (tp + bt) / 2
-        j = 0
-
-        do while ((abs(fptr(md)) .gt. eps).and.((tp-bt) .gt. eps))
-        !do while (abs(fptr(md)) .gt. eps)
-            md = (tp + bt) / 2
-            if (fptr(bt) * fptr(md) .gt. 0) then
-                bt = md
-            else if (fptr(bt) * fptr(md) .lt. 0) then
-                tp = md
-            end if
-            j = j + 1
-        end do
-
-        print *, "Bisection gives x =", md, "as a root, in", j , "steps"
-
-    end subroutine bisection
 
     pure real(8) function getWron(yleft, zleft, yright, zright)
         implicit none
         real(8), intent(in) :: yleft, zleft, yright, zright
-        getWron = yleft*zright-zleft*zright
+        getWron = yleft*zright - zleft*zright
     end function getWron
 
-    real(8) function getLog(param)
+    pure real(8) function getLog(yleft, zleft, yright, zright)
+        implicit none
+        real(8), intent(in) :: yleft, zleft, yright, zright
+        getLog = zright/yright - zleft/yleft
+    end function getLog
+
+    !y'=func(x, y, z); y = psi
+    !z'=gunc(x, y, z); zdx = dpsi
+
+    pure real(8) function func(x, y, z, nrg)
+        implicit none
+        real(8), intent(in) :: x, y, z, nrg
+        func = z
+    end function func
+
+    pure real(8) function gunc(x, y, z, nrg)
+        implicit none
+        real(8), intent(in) :: x, y, z, nrg
+        !ħ=m=1
+        gunc = 2.d0*(uptr(x)-nrg)*y
+    end function gunc
+
+    subroutine integL(nrg, yleft, zleft)
+        use config
         implicit none
 
-        abstract interface
-            pure real(8) function fct(x, y, z, param)
-            real(8), intent(in) :: x, y, z, param
-            end function fct
-        end interface
+        real(8), intent(inout) :: yleft, zleft
+        real(8), intent(in) :: nrg
 
-        procedure(fct), pointer :: fptr, gptr
-
-        real(8), intent(in) :: param
-        real(8) :: yleft, zleft, yright, zright
+        procedure(gunc), pointer :: fptr, gptr
+        real(8), dimension(4) :: k, l
+        real(8) :: x, step
+        integer :: i, j
 
         fptr => func
         gptr => gunc
 
-        call integL(fptr, gptr, -xinf, xcrs, param)
-        call integR(fptr, gptr, xcrs, xinf, param)
+        yleft = exp(-sqrt(-2.d0*nrg)*xinf)
+        zleft = sqrt(-2.d0*nrg)*exp(-sqrt(-2.d0*nrg)*xinf)
 
-        getLog = zright/yright - zleft/yleft
-    end function getLog
-
-    !y'=func(x, y, z); y ~ psi
-    !z'=gunc(x, y, z); z ~ dpsi/dx
-
-    pure real(8) function func(x, y, z, param)
-        implicit none
-        real(8), intent(in) :: x, y, z, param
-        func = z
-    end function func
-
-    pure real(8) function gunc(x, y, z, param)
-        implicit none
-        real(8), intent(in) :: x, y, z, param
-
-        gunc = 2.d0*(uptr(x)-param)*y
-        !ħ=m=1
-    end function gunc
-
-    subroutine integL(fptr, gptr, st, ed, param)
-        use config
-        implicit none
-
-        abstract interface
-            pure real(8) function fct(x, y, z, param)
-            real(8), intent(in) :: x, y, z, param
-            end function fct
-        end interface
-
-        procedure(fct), pointer, intent(in) :: fptr, gptr
-        real(8), intent(in) :: st, ed, param
-        real(8), dimension(4) :: k, l
-        real(8) :: x, step
-        integer :: i, j
-        real(8) :: yleft, zleft
-
-        yleft = exp(-sqrt(-2.d0*param)*xinf)
-        zleft = sqrt(-2.d0*param)*exp(-sqrt(-2.d0*param)*xinf)
-
-        step = abs(ed - st) / dots
+        step = (xcrs + xinf) / dots
 
         !open(unit = 1, file = "rkLeft.dat")
         do i = 1, dots+1
-            x = st + (i-1)*step
+            x = -xinf + (i-1)*step
             !write(1,*) x, yleft, zleft
-            k(1) = fptr(x, yleft, zleft, param)
-            l(1) = gptr(x, yleft, zleft, param)
+            k(1) = fptr(x, yleft, zleft, nrg)
+            l(1) = gptr(x, yleft, zleft, nrg)
 
-            k(2) = fptr(x+step/2, yleft+k(1)*step/2, zleft+k(1)*step/2, param)
-            l(2) = gptr(x+step/2, yleft+l(1)*step/2, zleft+l(1)*step/2, param)
+            k(2) = fptr(x+step/2, yleft+k(1)*step/2, zleft+k(1)*step/2, nrg)
+            l(2) = gptr(x+step/2, yleft+l(1)*step/2, zleft+l(1)*step/2, nrg)
 
-            k(3) = fptr(x+step/2, yleft+k(2)*step/2, zleft+k(2)*step/2, param)
-            l(3) = gptr(x+step/2, yleft+l(2)*step/2, zleft+l(2)*step/2, param)
+            k(3) = fptr(x+step/2, yleft+k(2)*step/2, zleft+k(2)*step/2, nrg)
+            l(3) = gptr(x+step/2, yleft+l(2)*step/2, zleft+l(2)*step/2, nrg)
 
-            k(4) = fptr(x+step, yleft+k(3)*step, zleft+k(3)*step, param)
-            l(4) = gptr(x+step, yleft+l(3)*step, zleft+l(3)*step, param)
+            k(4) = fptr(x+step, yleft+k(3)*step, zleft+k(3)*step, nrg)
+            l(4) = gptr(x+step, yleft+l(3)*step, zleft+l(3)*step, nrg)
 
             yleft = yleft + step*(k(1)+2*k(2)+2*k(3)+k(4))/6
             zleft = zleft + step*(l(1)+2*l(2)+2*l(3)+l(4))/6
@@ -222,43 +143,41 @@ contains
 
     end subroutine integL
 
-    subroutine integR(fptr, gptr, st, ed, param)
+    subroutine integR(nrg, yright, zright)
         use config
         implicit none
 
-        abstract interface
-            pure real(8) function fct(x, y, z, param)
-            real(8), intent(in) :: x, y, z, param
-            end function fct
-        end interface
+        real(8), intent(inout) :: yright, zright
+        real(8), intent(in) :: nrg
 
-        procedure(fct), pointer, intent(in) :: fptr, gptr
-        real(8), intent(in) :: st, ed, param
+        procedure(gunc), pointer :: fptr, gptr
         real(8), dimension(4) :: k, l
         real(8) :: x, step
         integer :: i, j
-        real(8) :: yright, zright
 
-        yright = exp(-sqrt(-2.d0*param)*xinf)
-        zright = sqrt(-2.d0*param)*exp(-sqrt(-2.d0*param)*xinf)
+        fptr => func
+        gptr => gunc
 
-        step = abs(ed - st) / dots
+        yright = exp(-sqrt(-2.d0*nrg)*xinf)
+        zright = sqrt(-2.d0*nrg)*exp(-sqrt(-2.d0*nrg)*xinf)
+
+        step = (xinf - xcrs) / dots
 
         !open(unit = 1, file = "rkRight.dat")
         do i = 1, dots+1
-            x = ed - (i-1)*step
+            x = xinf - (i-1)*step
             !write(1,*) x, yright, zright
-            k(1) = fptr(x, yright, zright, param)
-            l(1) = gptr(x, yright, zright, param)
+            k(1) = fptr(x, yright, zright, nrg)
+            l(1) = gptr(x, yright, zright, nrg)
 
-            k(2) = fptr(x-step/2, yright-k(1)*step/2, zright-k(1)*step/2, param)
-            l(2) = gptr(x-step/2, yright-l(1)*step/2, zright-l(1)*step/2, param)
+            k(2) = fptr(x-step/2, yright-k(1)*step/2, zright-k(1)*step/2, nrg)
+            l(2) = gptr(x-step/2, yright-l(1)*step/2, zright-l(1)*step/2, nrg)
 
-            k(3) = fptr(x-step/2, yright-k(2)*step/2, zright-k(2)*step/2, param)
-            l(3) = gptr(x-step/2, yright-l(2)*step/2, zright-l(2)*step/2, param)
+            k(3) = fptr(x-step/2, yright-k(2)*step/2, zright-k(2)*step/2, nrg)
+            l(3) = gptr(x-step/2, yright-l(2)*step/2, zright-l(2)*step/2, nrg)
 
-            k(4) = fptr(x-step, yright-k(3)*step, zright-k(3)*step, param)
-            l(4) = gptr(x-step, yright-l(3)*step, zright-l(3)*step, param)
+            k(4) = fptr(x-step, yright-k(3)*step, zright-k(3)*step, nrg)
+            l(4) = gptr(x-step, yright-l(3)*step, zright-l(3)*step, nrg)
 
             yright = yright - step*(k(1)+2*k(2)+2*k(3)+k(4))/6
             zright = zright - step*(l(1)+2*l(2)+2*l(3)+l(4))/6
@@ -268,5 +187,4 @@ contains
 
     end subroutine integR
 
-end program main
-
+end program
